03e8719a9857f92ca0939a34ea2da37a42236dfb
asachiyigor <asachiyigor@gmail.com>
2022-03-31 12:33:45 +0300

    2.4.2.1

---
+++ src/main/java/ru/netology/bonus/Main.java
@@ -0,0 +1,17 @@
+package ru.netology.bonus;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        System.out.println("actual = " + actual);
+        System.out.println("expected = " + expected);
+
+    }
+}
